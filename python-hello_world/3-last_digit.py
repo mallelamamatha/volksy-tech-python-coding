6a9d63703f2af6b5f7f228e2b9d6783b5a666cf0
root <you@example.com>
2022-12-27 05:06:28 +0000

    3

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    print(number,"greater than 5")
+elif number == 0:
+    print(number,"number is zero")
+else:
+    print(number,less than 6 and not 0")
