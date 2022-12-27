3d563a311d9d51e0929890567dddb8b626daaccc
root <you@example.com>
2022-12-27 10:13:44 +0000

    3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,9 +10,8 @@ if last == 0:
 else:
     if last > 5:
         line = ('Last digit of ' + str(number) +
-                ' is '+ str(last) + ' and is greater' + ' than 5')
+                ' is ' + str(last) + ' and is greater' + ' than 5')
     elif last < 5:
         line = ('Last digit of' + str(number) +
-                ' is '+ str(last) + 'and is less ' + 'than 6 and not 0')
+                ' is ' + str(last) + ' and is less ' + 'than 6 and not 0')
 print(line)
-
